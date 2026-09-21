d91e98d05b917077e70dccc2c495e959658f20ed
elise gurney <egurney2@wisc.edu>
2026-09-21 12:03:33 -0500

    renamed mak_jobs file and updated with our parameters

--- _code/mak_jobs2.R
+++ _code/mak_jobs2.R
@@ -4,11 +4,12 @@ source("https://github.com/jjcurtin/lab_support/blob/main/format_path.R?raw=true
 
 path_chtc <- format_path(str_c("cov/chtc/batch_", Sys.Date()))
 
-n_obs_lvls <- c(50, 100, 150, 200, 300, 400)
-n_covs_lvls <- c(4, 8, 12, 16, 20)
-p_good_covs_lvls <- c(0.25, 0.5, 0.75)
-r_ycov_lvls <- c(0.3, 0.5)    # correlation between y & good covs
-b_x_lvls <- c(0, 0.2, 0.5)    # effect of x on y
+n_obs_lvls <- c(100, 200, 300, 500)
+n_covs_lvls <- c(1, 2, 4)
+#p_good_covs_lvls <- c(0.25, 0.5, 0.75)
+r_ycov_lvls <- c(0.1, 0.3)    # correlation between y and covs
+r_xcov_lvls <- c(0.1, 0.3)    # correlation between x and covs
+b_x_lvls <- c(0, 0.3)    # correlation between x and y
 
 # we want 40,000 simulations
 # batching jobs at 500 sims per job
@@ -18,7 +19,8 @@ jobs <- expand_grid(n_sims = 500,
                     b_x = b_x_lvls,
                     n_covs = n_covs_lvls,
                     r_ycov = r_ycov_lvls,
-                    p_good_covs = p_good_covs_lvls,
+                    r_xcov = r_xcov_lvls,
+                    #p_good_covs = p_good_covs_lvls,
                     r_cov = 0.3) |> 
   slice(rep(1:n(), each = 80)) |> 
   mutate(job_num = row_number()) |> 
@@ -38,5 +40,5 @@ jobs |> write_csv(here::here(path_chtc, "input", "jobs.csv"),
 # copy other files
 file.copy("_code/cov.sh", here::here(path_chtc, "input"))
 file.copy("_code/cov.sub", here::here(path_chtc, "input"))
-file.copy("_code/fit_cov.R", here::here(path_chtc, "input"))
-file.copy("_code/fun_cov.R", here::here(path_chtc, "input"))
+file.copy("_code/fit_cov2.R", here::here(path_chtc, "input"))
+file.copy("_code/fun_cov2.R", here::here(path_chtc, "input"))
